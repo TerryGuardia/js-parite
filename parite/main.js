3420d0a9d496b5b9fde2c8caae87e08422513afd
Terry Guardia <74117979+TerryGuardia@users.noreply.github.com>
2023-01-09 18:13:14 +0100

    Add files via upload

---
+++ parite/main.js
@@ -0,0 +1,12 @@
+/*Verifie si le nombre est pair ou non
+* @param { Number }
+*/
+function isEven(nb) {
+    if (nb % 2 == 0) {
+        console.log("true");
+    }
+    else {
+        console.log("false");
+    }
+}
+isEven(12);
